3ce37b48303ac489515139ad8a0d8315de321144
Jacob M. Slone <jslone1993@gmail.com>
2015-01-22 16:29:29 -0500

    readme.md updated

--- template/proj/build.js
+++ template/proj/build.js
@@ -1,3 +1,4 @@
+// Load an image file from file = {name,data}
 function loadImage(file) {
   var script = [
     'var img = new Image();',
@@ -7,23 +8,29 @@ function loadImage(file) {
   return script;
 }
 
+// Load an audio file from file = {name,data}
 function loadAudio(file) {
   return 'module.exports = new Audio("data:' + this.mime + ';' + this.encoding + ',' + file.data + '");';
 }
 
+// Export build options
 module.exports = {
+  // Entry point for build
   entry :  {
-    js : 'src/main.js',
-    html : 'src/main.html'
+    js : 'src/main.js',       //the script to be called
+    html : 'src/main.html'    //will be used as template for final output
   },
+  // Output options
   output : {
-    html : '.tmp/build.html'
+    html : '.tmp/build.html'  //location to output final built project
   },
+  // Options for each file type
   files : {
     '.jpg' : {
-      mime : 'image/jpg',
-      encoding : 'base64',
-      load: loadImage
+      mime : 'image/jpg',   // mime type, used by loadImage
+      encoding : 'base64',  // encode type, used by sald, defaults to utf8
+      load: loadImage       // function which translates to node-style javascript
+                            // should be of type {name,data} -> string
     },
     '.png' : {
       mime : 'image/png',

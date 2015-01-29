794c2a90de9a6c4cf2909ab8255a123fcdf98645
lee7670 <lee7670@gmail.com>
2015-01-29 15:39:55 -0500

    fixed myimage to canvas

--- sald/mainloop.js
+++ sald/mainloop.js
@@ -9,7 +9,7 @@ if (typeof(window.sald) === "undefined") {
 }
 
 //Just 'require'-ing this file installs several useful global properties:
-window.sald.scene = {}; //the current scene; update, draw, and key functions will be called.
+window.sald.scene = {}; //the current scene; update, draw, scrollWheel,  and key functions will be called.
 window.sald.ctx = null; //the drawing context, call canvas 2d functions here
 window.sald.size = {x:320, y:240, mode:"exact"}; //set your desired size here
 window.sald.keys = {}; //all keys currently held down
@@ -267,12 +267,12 @@ function start(canvas) {
     
     if(canvas.addEventListener){
         //chrome/ie9/safari/opera
-        myimage.addEventListener("mousewheel", mouseWheel, false);
+        canvas.addEventListener("mousewheel", mouseWheel, false);
         //firefox
-        myimage.addEventListener("DOMMouseScroll", mouseWheel, false);
+        canvas.addEventListener("DOMMouseScroll", mouseWheel, false);
     }
     else{
-        myimage.attachEvent("onmousewheel", mouseWheel);
+        canvas.attachEvent("onmousewheel", mouseWheel);
     }
     function mouseWheel(e){
         var e = window.event || e;
